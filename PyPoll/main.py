a478a3a5a65521cee5b6677e53a9dd04ce6861c6
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-06-01 19:04:25 -0400

    PyPoll: Main analysis updated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ import os
 rowcount = 0
 votetally = []
 candidates = []
+wincount= 0
 
 election_data = os.path.join("Resources", "election_data.txt")
 
@@ -17,14 +18,21 @@ with open(election_data) as csvfile:
         rowcount = rowcount + 1
         votetally.append(row[2])
         
-
-candidates = set(votetally)
+candidates = set(votetally)      
 
 print(f"""
 
 Election Results
 ------------------------
 Total Votes: {rowcount}
+------------------------""")
+for cand in candidates:
+    percentage = round((votetally.count(cand)/rowcount)*100,3)
+    print(f'{cand}: {percentage}% ({votetally.count(cand)})')
+    if percentage > wincount:
+        winner = cand
+        wincount = percentage
+print(f"""------------------------
+Winner: {winner}
 ------------------------
-
 """)
