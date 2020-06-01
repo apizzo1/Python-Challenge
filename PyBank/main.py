032b11e6395d5ef92e77bb850b51a459ba3959d9
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-05-31 21:25:43 -0400

    Parts3 and 4 of PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,10 @@ import csv
 import os
 
 rowcount = 0
+Net_change = 0
 profit_counter = []
-Net_change = 0 
 change = []
+dates = []
 
 budget_path = os.path.join("Resources", "budget_data.csv")
 
@@ -17,6 +18,7 @@ with open(budget_path) as csvfile:
     for row in csvreader:
         rowcount = rowcount + 1
         profit_counter.append(int(row[1]))
+        dates.append(row[0])
         
 Net_change = sum(profit_counter)    
 
@@ -28,6 +30,11 @@ for i in range((len(profit_counter)) -1):
 
 average = round(sum(change)/len(change),2)
 
+max_inc = max(change)
+max_dec = min(change)
+inc_date = dates[len(dates) - (change.index(max_inc) +1)]
+dec_date = dates[len(dates) - (change.index(max_dec) +1)]
+
 #Summary of Analysis
 print(f"""
 Financial Analysis       
@@ -35,4 +42,6 @@ Financial Analysis
 Total Months: {rowcount} 
 Total: ${Net_change}
 Average Change: ${average}
+Greatest Increase in Profits: {inc_date} (${max_inc})
+Greatest Decrease in Profits: {dec_date} (${max_dec})
 """)
