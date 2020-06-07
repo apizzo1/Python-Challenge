f0c0928760ade1c51e643e8ee7397853d7b07beb
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-06-07 00:00:47 -0400

    Updating PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,54 +1,70 @@
 import csv
 import os
 
+#initialize variables
 rowcount = 0
-Net_change = 0
-profit_counter = []
-change = []
-dates = []
+profit_counter = 0
+max_inc = 0
+max_dec = 0
+sum_change = 0
 
+#import data file
 budget_path = os.path.join("Resources", "budget_data.csv")
 
+#Open and read data file
 with open(budget_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    #skip header
     csv_header = next(csvreader)
 
-    
     #count the number of months
     for row in csvreader:
         rowcount = rowcount + 1
-        profit_counter.append(int(row[1]))
-        dates.append(row[0])
-        
-Net_change = sum(profit_counter)    
+        #sum the net_change
+        profit_counter = profit_counter + int(row[1])
+        #must start at row 2 because we cannot subtract previous profit from row 1, since there is no previous profit before row 1
+        if rowcount >= 2:
+            #get the profit change between current and previous rows
+            current = int(row[1]) - prev_profit
+            #sum the changes to calculate average change later
+            sum_change = sum_change +current
+            #find the greatest profit increase and decrease
+            if current > max_inc:
+                max_inc = current
+                inc_date = row[0]
+            if current < max_dec:
+                max_dec = current
+                dec_date = row[0]
+        #set previous profit for next iteration
+        prev_profit = int(row[1])
 
-#Reverse list of profits/losses
-profit_counter.reverse()
-
-for i in range((len(profit_counter)) -1):
-    change.append(profit_counter[i] - profit_counter[i+1])
-
-average = round(sum(change)/len(change),2)
-
-max_inc = max(change)
-max_dec = min(change)
-inc_date = dates[len(dates) - (change.index(max_inc) +1)]
-dec_date = dates[len(dates) - (change.index(max_dec) +1)]
+#calculate average change 
+avg_change = round(sum_change/(rowcount-1),2) 
 
 #Summary of Analysis
+#create txt file to print to
+f= open("Analysis/Results_PyBank.txt","w")
+
+#write results to txt file
+f.write(f"""
+Financial Analysis       
+-------------------------     
+Total Months: {rowcount} 
+Total: ${profit_counter}
+Average Change: ${avg_change}
+Greatest Increase in Profits: {inc_date} (${max_inc})
+Greatest Decrease in Profits: {dec_date} (${max_dec})
+""")
+#close txt file
+f.close() 
+
+#print results to terminal
 print(f"""
 Financial Analysis       
 -------------------------     
 Total Months: {rowcount} 
-Total: ${Net_change}
-Average Change: ${average}
+Total: ${profit_counter}
+Average Change: ${avg_change}
 Greatest Increase in Profits: {inc_date} (${max_inc})
 Greatest Decrease in Profits: {dec_date} (${max_dec})
 """)
-
-output_data = os.path.join("Analysis","Results.csv")
-
-with open(output_data, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter = ',')
-        csvwriter.writerow(["Total Months", "Total", "Average Change","Greatest Increase in profits","Date of Greatest Increase in Profits","Greatest Decrease in Profits", "Date of Greatest Decrease in Profits"])
-        csvwriter.writerow([rowcount, Net_change, average,max_inc, inc_date, max_dec, dec_date])
